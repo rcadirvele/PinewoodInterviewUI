a2d745b77debc07ecbe24c6ac12ef78c39354b42
ram <rcadirvele@outlook.com>
2024-07-24 01:48:20 +0100

    Customer info Mudblazor ui - intial commit

---
+++ Pinewood.CustomerInfo.BlazorWasmUI/ViewModels/CustomerInfoVm.cs
@@ -0,0 +1,18 @@
+﻿namespace Pinewood.CustomerInfo.BlazorWasmUI.ViewModels
+{
+    public class CustomerInfoVm
+	{
+        public Guid Id { get; set; }
+
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Phone { get; set; }
+
+        public string? Postcode { get; set; }
+    }
+}
+
---
+++ Pinewood.CustomerInfo.BlazorWasmUI/ViewModels/Validations/CustomerInfoVmValidator.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+namespace Pinewood.CustomerInfo.BlazorWasmUI.ViewModels.Validatiors
+{
+    public class CustomerInfoVmValidator : AbstractValidator<CustomerInfoVm>
+    {
+		public CustomerInfoVmValidator()
+        {
+            RuleFor(x => x.FirstName)
+                .NotEmpty();
+
+            RuleFor(x => x.LastName)
+                .NotEmpty();
+
+            RuleFor(x => x.Email)
+                .EmailAddress();
+
+            RuleFor(x => x.Phone)
+                .NotEmpty()
+                .Length(10);
+
+            RuleFor(x => x.Postcode)
+                .MinimumLength(6)
+                .MaximumLength(10);
+        }
+
+        public Func<object, string, Task<IEnumerable<string>>> ValidateValue => async (model, propertyName) =>
+        {
+            var result = await ValidateAsync(ValidationContext<CustomerInfoVm>.CreateWithOptions((CustomerInfoVm)model, x => x.IncludeProperties(propertyName)));
+            if (result.IsValid)
+                return Array.Empty<string>();
+            return result.Errors.Select(e => e.ErrorMessage);
+        };
+
+    }
+}
+
